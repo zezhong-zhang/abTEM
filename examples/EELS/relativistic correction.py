78895d45b8f804a49a8cc91c11833b28a3ad8442
zezhong-zhang <jack.zezhong.zhang@gmail.com>
2022-01-20 22:49:30 +0100

    delete extra eels fig

--- examples/EELS/relativistic correction.py
+++
@@ -1,7 +0,0 @@
-def schroedinger_derivative_relativistic_correct(y, r, l, e, vr):
-            # fine structure constant
-            alpha = 7.2973525693e-3 
-            (u, up) = y
-            dvdr = (vr.derivative(n=1)(r)-vr(r)/r)/r
-            # note vr is effective potential multiplied by radius:
-            return np.array([up, (l * (l + 1) / r ** 2 + 2 * vr(r) / r - e - alpha**2 / 4 * ((e-2*vr(r)/r)**2+2*l/r*dvdr)) * u - alpha**2 /4 * dvdr * (up*r-u)/r])
